431b6f239fb64c099135b7dbef4bd22276150e17
Shak <shak_corp@zohomail.in>
2025-11-14 20:08:06 +0530

    Update startup.sh

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-# Kill old sessions
+# Kill session if exists
 vncserver -kill :1 >/dev/null 2>&1
 
-# Start VNC with GUI desktop
+# Start XFCE desktop session
+echo "#!/bin/sh
+xrdb \$HOME/.Xresources
+startxfce4 &" > /root/.vnc/xstartup
+chmod +x /root/.vnc/xstartup
+
+# Start VNC
 vncserver :1 -geometry 1280x800 -depth 24
 
-# Keep container alive
+# Keep alive
 while true; do sleep 1000; done
